25d28701d0c5f0937be5b2dfc9f54b44e327bab6
DavBaquero <david.baquero.19@gmail.com>
2024-11-07 18:02:52 +0100

    Refactor(): clean up commit

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ import conexionserver
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
+        '''
+            zona de inicializar de las tablas
+        '''
         super(Main, self).__init__()
         var.ui = Ui_venPrincipal()
         var.ui.setupUi(self)
@@ -25,23 +28,29 @@ class Main(QtWidgets.QMainWindow):
         self.setStyleSheet(styles.load_stylesheet())
         conexion.Conexion.db_conexion(self)
         # conexionserver.ConexionServer.crear_conexion()
+
+        '''
+            zona de inicializar eventos
+        '''
         eventos.Eventos.cargarProvincias(self)
-        eventos.Eventos.cargarProvprop(self)
         eventos.Eventos.cargarMunicipio(self)
+
+        eventos.Eventos.cargarProvprop(self)
         eventos.Eventos.cargarMuniprop(self)
-        var.ui.cmbProvprop.currentIndexChanged.connect(eventos.Eventos.cargarMuniprop)
         eventos.Eventos.cargarTipoprop()
-        clientes.Clientes.cargaTablaClientes(self)
 
         '''
         zona de eventos de las tablas
         '''
+        clientes.Clientes.cargaTablaClientes(self)
+        propiedades.Propiedades.cargarTablaPropiedades()
+
         eventos.Eventos.resizeTablaClientes(self)
+        eventos.Eventos.resizeTablaPropiedades(self)
+
+        var.ui.tablaProp.clicked.connect(propiedades.Propiedades.cargaOnePropiedad)
         var.ui.tablaClientes.clicked.connect(clientes.Clientes.cargaOneCliente)
 
-        propiedades.Propiedades.cargarTablaPropiedades()
-        eventos.Eventos.resizeTablaPropiedades(self)
-        var.ui.tablaProp.clicked.connect(propiedades.Propiedades.cargaOnePropiedad)
         '''
         zona de eventos del menubar
         '''
@@ -54,36 +63,44 @@ class Main(QtWidgets.QMainWindow):
         eventos de botones
         '''
         var.ui.btnGrabarcli.clicked.connect(clientes.Clientes.altaCliente)
-        var.ui.btnAltacli.clicked.connect(lambda: eventos.Eventos.abrirCalendar(0,0))
-        var.ui.btnBajacli.clicked.connect(lambda: eventos.Eventos.abrirCalendar(0,1))
-        var.ui.btnAltaprop.clicked.connect(lambda: eventos.Eventos.abrirCalendar(1,0))
-        var.ui.btnBajaprop.clicked.connect(lambda: eventos.Eventos.abrirCalendar(1,1))
         var.ui.btnModificli.clicked.connect(clientes.Clientes.modifCliente)
         var.ui.btnEliminarcli.clicked.connect(clientes.Clientes.bajaCliente)
+        var.ui.btnAltacli.clicked.connect(lambda: eventos.Eventos.abrirCalendar(0,0))
+        var.ui.btnBajacli.clicked.connect(lambda: eventos.Eventos.abrirCalendar(0,1))
+
         var.ui.btnEliminarprop.clicked.connect(propiedades.Propiedades.bajaProp)
         var.ui.btnGrabarprop.clicked.connect(propiedades.Propiedades.altaPropiedad)
         var.ui.btnModifiprop.clicked.connect(propiedades.Propiedades.modifProp)
+        var.ui.btnAltaprop.clicked.connect(lambda: eventos.Eventos.abrirCalendar(1, 0))
+        var.ui.btnBajaprop.clicked.connect(lambda: eventos.Eventos.abrirCalendar(1, 1))
+
         '''
         eventos de cajas de texto
         '''
         var.ui.txtDnicli.editingFinished.connect(lambda: clientes.Clientes.checkDNI(var.ui.txtDnicli.text()))
         var.ui.txtEmailcli.editingFinished.connect(lambda: clientes.Clientes.checkEmail(var.ui.txtEmailcli.text()))
         var.ui.txtMovilcli.editingFinished.connect(lambda: clientes.Clientes.checkMovil(var.ui.txtMovilcli.text()))
+
         var.ui.txtMovilprop.editingFinished.connect(lambda : propiedades.Propiedades.checkMovilProp(var.ui.txtMovilprop.text()))
         '''
         eventos combobox
         '''
+
         var.ui.cmbProvcli.currentIndexChanged.connect(eventos.Eventos.cargarMunicipio)
+
+        var.ui.cmbProvprop.currentIndexChanged.connect(eventos.Eventos.cargarMuniprop)
+
         '''
         zona toolbar
         '''
-
         var.ui.actionbarSalir.triggered.connect(eventos.Eventos.mensajeSalir)
         var.ui.actionbarLimpar.triggered.connect(eventos.Eventos.limpiarPanel)
+
         '''
         zona eventos checkbox
         '''
         var.ui.chkHistoriacli.stateChanged.connect(clientes.Clientes.historicoCli)
+
         var.ui.chkHistoriaprop.stateChanged.connect(propiedades.Propiedades.historicoProp)
 
 if __name__ == '__main__':

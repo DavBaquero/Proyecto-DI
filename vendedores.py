c86d0554cc81fd51170c43f4c61da6c758f67811
DavBaquero <david.baquero.19@gmail.com>
2025-01-27 09:51:48 +0100

    Feat(): add cargar oneVendedor en ventas

--- vendedores.py
+++ vendedores.py
@@ -188,6 +188,7 @@ class Vendedores:
                 else:
                     listado[i].setText(registro[i])
             #Clientes.cargaCliente(registro)
+            var.ui.txtidvenfac.setText(str(registro[0]))
 
         except Exception as error:
             print("Error carga un vendedor ", error)
